3e3efa85c71a6ee0521f86bf3ad88138d232b4ef
Thomas Theunen <thomas.theunen@forward.eu>
2022-08-01 22:06:56 +0200

    Add endpoint to test the session bridge

---
+++ cartridges/app_api_base/cartridge/controllers/SessionBridge.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/**
+ * @namespace SessionBridge
+ */
+
+var server = require('server');
+
+/**
+ * SessionBridge-Test : This endpoint is to test the Session Bridge.
+ * @name Base/SessionBridge-Test
+ * @function
+ * @memberof SessionBridge
+ * @param {category} - sensitive
+ * @param {renders} - json
+ * @param {serverfunction} - get
+ */
+server.get('Test', function (req, res, next) {
+    var System = require('dw/system/System');
+    if ((System.getInstanceType() !== System.PRODUCTION_SYSTEM)
+        && (System.getInstanceType() !== System.STAGING_SYSTEM)) {
+        res.json({
+            customer_id: req.currentCustomer.raw.ID,
+            customer_no: req.currentCustomer.profile ? req.currentCustomer.profile.customerNo : null
+        });
+    }
+
+    next();
+});
+
+module.exports = server.exports();
